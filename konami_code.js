4d1c4413c93daf81ebea9872f0e2e3e66b2ddb60
cmonkey03 <russellpbrown@gmail.com>
2018-09-05 01:49:31 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,18 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0
+
+  document.body.addEventListener('keydown', function(event) {
+    const key = event.key
+    if (key === codes[index]) {
+      index++
+    } else {
+      index = 0
+    }
+    if (index === codes.length) {
+      window.alert('Congratulations')
+      index = 0
+    }
+  });
 }
